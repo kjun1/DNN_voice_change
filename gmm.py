8269179b4286fa84c278d5166582cc050125b220
kjun1 <p.k.maejima1211@icloud.com>
2020-12-15 00:18:29 +0900

    JVS盤gmm

---
+++ gmm.py
@@ -0,0 +1,9 @@
+import sidekit
+import os
+import sys
+import multiprocessing
+import matplotlib.pyplot as mpl
+import logging
+import numpy as np
+
+logging.basicConfig(filename='log/ubm-gmm.log',level=logging.DEBUG)
